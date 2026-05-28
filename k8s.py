33073f10b16e6a248abe0e935c4b3173393b3fa9
Maxime Moreillon <m.moreillon@gmail.com>
2026-05-28 13:30:06 +0900

    in-cluster k8s config

--- k8s.py
+++ k8s.py
@@ -1,5 +1,8 @@
 from kubernetes import client, config
 
-config.load_kube_config()
+try:
+    config.load_incluster_config()
+except config.ConfigException:
+    config.load_kube_config()
 
 v1 = client.CoreV1Api()
